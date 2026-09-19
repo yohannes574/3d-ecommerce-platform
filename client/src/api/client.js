5864d3471150032b8d5819c0940088389c61b85e
Yohannes <yohannesjj33@gmail.com>
2026-09-19 07:24:40 +0300

    Connect frontend to production API

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const TOKEN_KEY = 'voltix_token';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem(TOKEN_KEY);
